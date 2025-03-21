fe06c183271126dd3c188414e337a32498d1ebef
Lucas Libanio <mac0321@ludovico.cec.ime.usp.br>
2025-03-21 10:15:15 -0300

    resolvi le bombe RECEBA

--- lerolero.py
+++ lerolero.py
@@ -2,13 +2,13 @@
 '''GERADOR DE LERO LOUR GERA FRASES DE EFEITO''' 
 from random import choice
 
+parte1 = []
+parte2 = []
+parte3 = []
+
 lingua = int(input("escolha o linguini: 1 - portugues, 2 - ingles")
 
 if lingua == 2:
-    parte1 = []
-    parte2 = []
-    parte3 = []
-else:
     parte1 = []
     parte2 = []
     parte3 = []
