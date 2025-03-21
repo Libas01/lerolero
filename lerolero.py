3debc61706e3cbf733eb7536a3f54047dbb1c2e7
Lucas Libanio <mac0321@ludovico.cec.ime.usp.br>
2025-03-21 10:13:13 -0300

    esqueleto assustador (medo em ingles)

--- lerolero.py
+++ lerolero.py
@@ -2,8 +2,15 @@
 '''GERADOR DE LERO LOUR GERA FRASES DE EFEITO''' 
 from random import choice
 
-parte1 = []
-parte2 = []
-parte3 = []
+lingua = int(input("escolha o linguini: 1 - portugues, 2 - ingles")
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+else:
+    parte1 = []
+    parte2 = []
+    parte3 = []
 
 print(choice(parte1),choice(parte2),choice(parte3))
