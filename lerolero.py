bb3d885b613c673d909b3697c64cd1d196234b45
alexys.alves@usp.br <mac0321@ludovico.cec.ime.usp.br>
2025-03-21 10:18:15 -0300

    adicionei mensagens iditoticas ao programa

--- lerolero.py
+++ lerolero.py
@@ -2,8 +2,14 @@
 '''GERADOR DE LERO LOUR GERA FRASES DE EFEITO''' 
 from random import choice
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["O sistema em desenvolvimento",
+"O novo protocolo de comunicacao",
+"O algoritmo otimizado"]
+parte2 = ["possui excelente desempenho",
+"oferece garantias de precisao acima da media",
+"preenche uma lacuna significativa"]
+parte3 = ["nas aplicacoes a que se destina",
+"em relacao as opcoes disponiveis no mercado",
+", provendo ampla vantagem competitiva a seus usuarios"]
 
 print(choice(parte1),choice(parte2),choice(parte3))
