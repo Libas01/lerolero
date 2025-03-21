51f26d5a25409493b6b593fb5676dcdac6287e71
alexys.alves@usp.br <mac0321@ludovico.cec.ime.usp.br>
2025-03-21 09:57:58 -0300

    fiz um programa idiotico em python

--- lerolero.py
+++ lerolero.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 '''GERADOR DE LERO LOUR GERA FRASES DE EFEITO''' 
+from random import choice
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(choice(parte1),choice(parte2),choice(parte3))
